c921647a2b2308e18a62217a38acd4cc4eaad0be
Phil J. Laszkowicz <phil@fillip.pro>
2017-11-24 12:41:42 +0200

    Restructured package to support cleaner separation of modules.

--- Package.swift
+++ Package.swift
@@ -2,6 +2,81 @@
 
 import PackageDescription
 
+let modelTargets: [Target] = [
+    .target(
+        name: "KauppaAccountsModel",
+        dependencies: ["KauppaCore"],
+        path: "Sources/KauppaAccounts/Model"
+    ),
+    .target(
+        name: "KauppaOrdersModel",
+        dependencies: ["KauppaCore", "KauppaProductsModel"],
+        path: "Sources/KauppaOrders/Model"
+    ),
+    .target(
+        name: "KauppaProductsModel",
+        dependencies: ["KauppaCore"],
+        path: "Sources/KauppaProducts/Model"
+    )
+]
+
+let serviceTargets: [Target] = [
+    .target(
+        name: "KauppaAccountsService",
+        dependencies: ["KauppaAccountsModel", "KauppaCore"],
+        path: "Sources/KauppaAccounts/Service"
+    )
+]
+
+let daemonTargets: [Target] = [
+    .target(
+        name: "KauppaAccounts",
+        dependencies: ["KauppaAccountsService", "KauppaAccountsModel", "KauppaCore"],
+        exclude: ["Service", "Model"]
+    ),
+    .target(
+        name: "KauppaOrders",
+        dependencies: ["KauppaOrdersModel", "KauppaCore"],
+        exclude: ["Model"]
+    ),
+    .target(
+        name: "KauppaProducts",
+        dependencies: ["KauppaProductsModel", "KauppaCore"],
+        exclude: ["Model"]
+    ),
+    .target(
+        name: "KauppaTax",
+        dependencies: ["KauppaCore"]
+    )
+]
+
+let testTargets: [Target] = [
+    .testTarget(
+        name: "KauppaAccountsTests",
+        dependencies: ["KauppaAccountsService", "KauppaAccountsModel", "KauppaCore"]),
+    .testTarget(
+        name: "KauppaOrdersTests",
+        dependencies: ["KauppaOrders", "KauppaCore"]),
+    .testTarget(
+        name: "KauppaProductsTests",
+        dependencies: ["KauppaProducts", "KauppaCore"]),
+    .testTarget(
+        name: "KauppaTaxTests",
+        dependencies: ["KauppaTax", "KauppaCore"])
+]
+
+var targets: [Target] = [
+    .target(
+        name: "KauppaCore",
+        dependencies: []
+    )
+]
+
+targets.append(contentsOf: modelTargets)
+targets.append(contentsOf: serviceTargets)
+targets.append(contentsOf: daemonTargets)
+targets.append(contentsOf: testTargets)
+
 let package = Package(
     name: "Kauppa",
     products: [
@@ -25,57 +100,5 @@ let package = Package(
     dependencies: [
         
     ],
-    targets: [
-        .target(
-            name: "KauppaCore",
-            dependencies: []),
-        .target(
-            name: "KauppaAccounts",
-            dependencies: ["KauppaAccountsService", "KauppaAccountsModel", "KauppaCore"],
-            exclude: ["Service", "Model"]
-        ),
-        .target(
-            name: "KauppaAccountsService",
-            dependencies: ["KauppaAccountsModel", "KauppaCore"],
-            path: "Sources/KauppaAccounts/Service"
-        ),
-        .target(
-            name: "KauppaAccountsModel",
-            dependencies: ["KauppaCore"],
-            path: "Sources/KauppaAccounts/Model"
-        ),
-        .target(
-            name: "KauppaOrders",
-            dependencies: ["KauppaOrdersModel", "KauppaCore"],
-            exclude: ["Model"]
-        ),
-        .target(
-            name: "KauppaOrdersModel",
-            dependencies: ["KauppaCore", "KauppaProductsModel"],
-            path: "Sources/KauppaOrders/Model"
-        ),
-        .target(
-            name: "KauppaProducts",
-            dependencies: ["KauppaProductsModel", "KauppaCore"],
-            exclude: ["Model"]),
-        .target(
-            name: "KauppaProductsModel",
-            dependencies: ["KauppaCore"],
-            path: "Sources/KauppaProducts/Model"),
-        .target(
-            name: "KauppaTax",
-            dependencies: ["KauppaCore"]),
-        .testTarget(
-            name: "KauppaAccountsTests",
-            dependencies: ["KauppaAccountsService", "KauppaAccountsModel", "KauppaCore"]),
-        .testTarget(
-            name: "KauppaOrdersTests",
-            dependencies: ["KauppaOrders", "KauppaCore"]),
-        .testTarget(
-            name: "KauppaProductsTests",
-            dependencies: ["KauppaProducts", "KauppaCore"]),
-        .testTarget(
-            name: "KauppaTaxTests",
-            dependencies: ["KauppaTax", "KauppaCore"])
-    ]
+    targets: targets
 )
