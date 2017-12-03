069b51f8cd9009237c6423ecb77ab7a09fb6bd85
Ravi Shankar <wafflespeanut@gmail.com>
2017-12-03 22:28:16 +0530

    Add tests for orders

--- Tests/KauppaOrdersTests/Service/TestOrdersService.swift
+++ Tests/KauppaOrdersTests/Service/TestOrdersService.swift
@@ -1,6 +1,7 @@
 import Foundation
 import XCTest
 
+import KauppaCore
 import KauppaProductsModel
 @testable import KauppaOrdersModel
 @testable import KauppaOrdersRepository
@@ -26,10 +27,29 @@ class TestOrdersService: XCTestCase {
         let store = TestStore()
         let repository = OrdersRepository(withStore: store)
         let productsService = TestProductsService()
-        let productData = ProductData(title: "", subtitle: "", description: "")
-        let product = productsService.createProduct(data: productData)
+        var productData = ProductData(title: "", subtitle: "", description: "")
+        productData.inventory = 5
+        productData.price = 3.0
+        productData.weight = UnitMeasurement(value: 5.0, unit: .gram)
+        let product = productsService.createProduct(data: productData)!
         let ordersService = OrdersService(withRepository: repository,
                                           productsService: productsService)
-        //
+
+        let inventoryUpdated = expectation(description: "product inventory updated")
+        productsService.callbacks[product.id] = { patch in
+            XCTAssertEqual(patch.inventory, 2)
+            inventoryUpdated.fulfill()
+        }
+
+        let orderData = OrderData(products: [OrderUnit(id: product.id, quantity: 3)])
+        let order = ordersService.createOrder(data: orderData)!
+        XCTAssertNotNil(order.id)
+        XCTAssertEqual(order.totalItems, 3)
+        XCTAssertEqual(order.totalWeight.value, 15.0)
+        XCTAssertEqual(order.totalPrice, 9.0)
+
+        waitForExpectations(timeout: 2) { error in
+            XCTAssertNil(error)
+        }
     }
 }
